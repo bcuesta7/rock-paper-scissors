3e55bb63441f1addb07572b7e6664c22732b8078
Blanca <49519645+bcuesta7@users.noreply.github.com>
2024-02-21 11:11:19 +0100

    Write Pseudocode

---
+++ javascript.js
@@ -0,0 +1,14 @@
+//Rock Paper Scissors
+
+/*
+1. Computer choices one option (Rock, Paper, Scissors)
+2. User inputs "Rock", "Paper" or "Scissors"
+3. If computer wins return "You Loose! [computer choice] wins [player choice]"
+4. If player wins return "You Win! [player choice] wins [computer choice]"
+5. If computer selection and player selection are equal, return "Tie!"
+
+Rock beats Scissors
+Paper beats Rock
+Scissors beats Paper
+
+*/
